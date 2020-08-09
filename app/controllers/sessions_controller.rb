7a55b85c70a9d964c3738bece08d8cd44fbd8fa9
matthewchao <matthew.chao1@yahoo.com>
2020-08-08 22:37:34 -0400

    behavior for successful or unsuccessful logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,20 @@ class SessionsController < ApplicationController
     puts params[:email] #this successfully logs the passed-in email
     puts params[:password] #this doesn't work, how does rails know not to do this??
     # on the other hand debug(params) exposes it...
-    @user = User.find_by email: params[:email] #but replace this 
-    if @user
+    @user = User.find_by email: params[:session][:email] #but replace this 
+    # recall that .authenticate is automatically provided by using has_secure_passowrd
+    # on the user model
+    if @user && @user.authenticate( params[:session][:password] )
       #successful session creation here
-      render :new
+      puts 'good login'
+      flash[:notice] = 'Logged in!'
+      redirect_to @user
     else
-      redirect_to root_url
+      puts 'bad login'
+      # note the use of flash.now; this is because render doesn't count as a "request"
+      # and so the user would 
+      flash[:alert] = 'Invalid combination'
+      render :new
       # re-render the sessions new form,
       # understanding that the 
     end
