9c107fab93d8f8684f572fc82406b1a39bc46d99
matthewchao <matthew.chao1@yahoo.com>
2020-08-22 10:43:01 -0400

    add remember_token via has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_secure_password :remember_token, validations: false
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
